9e172da74d4ae5e791d505df10b203e0520ae21c
Ishanth <hombaiah01@gmail.com>
2025-07-28 10:46:44 -0400

    Finalized project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # pomodoro-timer
+# This timer script times work and break sessions for the Pomodoro technique, which involves alternating between 25 minutes of work and 5 minutes of break time.
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import time
 import winsound
+import sys
 
-def timer_start():
-    time_intervals = [25 * 60, 5 * 60] # 25 minutes of work and 5 minutes of break time
-    currInterval = 0
-    currSession = 0
+def timer_start(workTime = 25, breakTime=5):
+    time_intervals = [workTime * 60, breakTime * 60] # Default is 25 minutes of work and 5 minutes of break time
+    intervalType = 0 # Whether the current session is for working or taking a break
+    workSessionCount = 0
 
     print("The timer has begun! Use Ctrl-C to stop the timer.")
     try: 
-        while True:    
-            currSession += 1
-            time.sleep(time_intervals[currInterval])
-            if currInterval == 0:
-                print(f"Work session {currSession} complete! Take a 5 minute break.")
+        while True:
+            if intervalType == 0: # If current session is work, increment workSessionCount    
+                workSessionCount += 1 
+            time.sleep(time_intervals[intervalType]) 
+            if intervalType == 0:
+                print(f"Work session {workSessionCount} complete! Take a {breakTime} minute break.")
             else: 
-                print(f"Break time over! Time for another 25 minutes of work.")
-            currInterval = 1 - currInterval # Switch from study to break mode and vice versa
-            winsound.Beep(1000, 500)
+                print(f"Break time over! Time for another {workTime} minutes of work.")
+            intervalType = 1 - intervalType # Switch from study to break mode and vice versa
+            winsound.Beep(1000, 1000) 
 
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: # Ctrl-C in terminal
         print("Timer stopped!")
 
-timer_start()
+try: 
+    workTime, breakTime = int(sys.argv[1]), int(sys.argv[2]) # If user specifies custom work and break times
+except:
+    workTime, breakTime = 25, 5
+
+timer_start(workTime, breakTime)
