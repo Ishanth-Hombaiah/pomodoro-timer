15741377e9a98ce8995e5aace05b6ce544ed6bd9
Ishanth-Hombaiah <hombaiah01@gmail.com>
2026-01-22 13:46:36 -0800

    First iteration

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import time
 import winsound
 import sys
+from datetime import datetime
 
 def timer_start(workTime=25, breakTime=5, top_of_30=False):
     time_intervals = [workTime * 60, breakTime * 60] # Default is 25 minutes of work and 5 minutes of break time
     intervalType = 0 # Whether the current session is for working or taking a break
     workSessionCount = 0
     
-
     print("The timer has begun! Use Ctrl-C to stop the timer.")
     winsound.Beep(1000, 1000) 
     
-    try: 
+    if top_of_30:
+        curr_time = datetime.now().time()
+        if curr_time[1] != 30 and curr_time[1] != 0:
+            time.sleep(abs(30 - curr_time[1]))
+    
+    try:
         while True:
             if intervalType == 0: # If current session is work, increment workSessionCount    
                 workSessionCount += 1 
